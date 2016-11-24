3b03d8fb0a10485b9e7479942dcce39de957c328
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-24 23:57:03 +0100

    added tests for getting to the next voting functionality

--- test/core.spec.js
+++ test/core.spec.js
@@ -32,6 +32,50 @@ describe('application logic', () => {
                 entries: [ 'Sunshine' ],
             }));
         });
+
+        it('puts winner of current vote back to entries', () => {
+            const state = fromJS({
+                vote: {
+                    pair: [ 'Trainspotting', '28 Days Later' ],
+                    tally: {
+                        'Trainspotting': 4,
+                        '28 Days Later': 2,
+                    },
+                },
+                entries: [ 'Sunshine', 'Millions', '127 Hours' ],
+            });
+
+            const nextState = next(state);
+
+            expect(nextState).to.equal(fromJS({
+                vote: {
+                    pair: [ 'Sunshine', 'Millions' ],
+                },
+                entries: [ '127 Hours', 'Trainspotting' ],
+            }));
+        });
+
+        it('puts both from tied vote back to entries', () => {
+            const state = fromJS({
+                vote: {
+                    pair: [ 'Trainspotting', '28 Days Later' ],
+                    tally: {
+                        'Trainspotting': 3,
+                        '28 Days Later': 3,
+                    },
+                },
+                entries: [ 'Sunshine', 'Millions', '127 Hours' ],
+            });
+
+            const nextState = next(state);
+
+            expect(nextState).to.equal(fromJS({
+                vote: {
+                    pair: [ 'Sunshine', 'Millions' ],
+                },
+                entries: [ '127 Hours', 'Trainspotting', '28 Days Later' ],
+            }));
+        });
     });
 
     describe('vote', () => {
