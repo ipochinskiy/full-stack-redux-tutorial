3d01e2aa255171c5b1ebd81d22688beacc5cae55
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-25 00:00:52 +0100

    refactored #next() to support getting to the next voting round functionality

--- src/core.js
+++ src/core.js
@@ -4,8 +4,32 @@ export const setEntries = (state, entries) => {
     return state.set('entries', List(entries));
 }
 
+const getWinners = (vote) => {
+    if (!vote) {
+        return [];
+    };
+
+    const [ a, b ] = vote.get('pair');
+    const aVotes = vote.getIn([ 'tally', a ], 0);
+    const bVotes = vote.getIn([ 'tally', b ], 0);
+
+    const winners = [];
+
+    if (aVotes >= bVotes) {
+        winners.push(a);
+    }
+
+    if (aVotes <= bVotes) {
+        winners.push(b);
+    }
+
+    return winners;
+}
+
 export const next = (state) => {
-    const entries = state.get('entries');
+    const winners = getWinners(state.get('vote'));
+    const entries = state.get('entries').concat(winners);
+
     return state.merge({
         vote: Map({
             pair: entries.take(2),
