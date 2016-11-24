ab4984190f7aa7652edc19aaf4f88dc5ea0a803b
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-24 22:21:52 +0100

    implemented starting a vote function

--- src/core.js
+++ src/core.js
@@ -1,5 +1,15 @@
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
 export const setEntries = (state, entries) => {
-  return state.set('entries', List(entries));
+    return state.set('entries', List(entries));
+}
+
+export const next = (state) => {
+    const entries = state.get('entries');
+    return state.merge({
+        vote: Map({
+            pair: entries.take(2),
+        }),
+        entries: entries.skip(2),
+    });
 }
