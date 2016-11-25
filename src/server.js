03cff7b4cbcaed904dd4f2ba30973f5bc7bef4c6
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-25 21:09:55 +0100

    added subscription to actions produced by a client

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ const startServer = (store) => {
 
     io.on('connection', (socket) => {
         socket.emit('state', store.getState().toJS());
+        socket.on('action', store.dispatch.bind(store));
     });
 };
 
