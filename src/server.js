a55805ee33cd5158f31d7915e2f48e96fd414cfe
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-25 21:02:16 +0100

    added emitting the state on every connection and state change

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 import Server from 'socket.io';
 
-const startServer = () => {
-  const io = new Server().attach(8090);
+const startServer = (store) => {
+    const io = new Server().attach(8090);
+
+    store.subscribe(
+        () => io.emit('state', store.getState().toJS())
+    );
+
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS());
+    });
 };
 
 export default startServer;
