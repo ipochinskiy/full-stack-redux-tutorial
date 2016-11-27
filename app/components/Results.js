0ceeb36338a15b91dc9d6d604b6d92604ccaabeb
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-28 00:53:33 +0100

    added dummy Results component

---
+++ app/components/Results.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+export default class Results extends PureComponent {
+    render() {
+        return (
+            <div>Hello from results!</div>
+        );
+    }
+}
