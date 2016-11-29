0e8a6b250953ac7cdf730b19c9cebe8d2e1a692d
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-29 02:10:17 +0100

    added socket.io-client

--- app/index.js
+++ app/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import io from 'socket.io-client';
+
 import { Router, Route, hashHistory } from 'react-router';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+
+import reducer from './reducer';
 
 import App from './components/App';
 import { ResultsContainer } from './components/Results';
 import { VotingContainer } from './components/Voting';
+
+const store = createStore(reducer);
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on(
+    'state',
+    state => store.dispatch({
+        type: 'SET_STATE',
+        state,
+    })
+);
 
 const routes = (
     <Route component={App}>
