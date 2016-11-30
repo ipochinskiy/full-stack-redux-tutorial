8774f13b673bbb88778dfa5a31372d0f1ea9f812
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-11-30 23:43:45 +0100

    implemented VOTE action handling by the reducer

--- app/reducer.js
+++ app/reducer.js
@@ -2,10 +2,19 @@ import { Map } from 'immutable';
 
 const setState = (state, newState) => state.merge(newState);
 
+const vote = (state, entry)  => {
+    const currentPair = state.getIn([ 'vote', 'pair' ]);
+    return currentPair && currentPair.includes(entry) ?
+        state.set('hasVoted', entry) :
+        state;
+}
+
 export default function(state = Map(), action) {
     switch (action.type) {
         case 'SET_STATE':
             return setState(state, action.state);
+        case 'VOTE':
+            return vote(state, action.entry);
         default:
             return state;
     }
