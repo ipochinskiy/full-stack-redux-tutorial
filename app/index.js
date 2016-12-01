5e2f8d16314f8b1c04aa036a0b09d5ae8ccf0bd6
Ilia Pochinskii <zorkiypnz@yandex.ru>
2016-12-01 01:27:59 +0100

    fixed not dispatching the state got from the server

--- app/index.js
+++ app/index.js
@@ -19,13 +19,13 @@ const store = createStore(reducer);
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on(
     'state',
-    state => setState(state)
+    state => store.dispatch(setState(state))
 );
 
 const routes = (
     <Route component={App}>
-        <Route path='/results' component={ResultsContainer} />
         <Route path='/' component={VotingContainer} />
+        <Route path='/results' component={ResultsContainer} />
     </Route>
 );
 
